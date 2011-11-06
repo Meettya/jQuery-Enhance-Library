4cf2f78ea7bfc13868839da127ad39b1756760bb
Meettya <meettya@gmail.com>
2011-11-06 14:11:11 +0300

    * add some things to doc update

--- doc_update.sh
+++ doc_update.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+doc_dir='doc'
+tmp_message='tmp_mess'
 parent_sha=$(git show-ref -s refs/heads/gh-pages)
-doc_sha=$(git ls-tree -d HEAD doc | awk '{print $3}')
-new_commit=$(echo "Auto-update docs." | git commit-tree $doc_sha -p $parent_sha)
+doc_sha=$(git ls-tree -d HEAD $doc_dir | awk '{print $3}')
+git log --pretty=format:'%s' -n 1 $doc_dir > $tmp_message
+new_commit=$(echo "Auto-update docs." | git commit-tree $doc_sha -p $parent_sha < $tmp_message )
+rm $tmp_message
 git update-ref refs/heads/gh-pages $new_commit
