48b901deaec2cf56720ac24b7d051e0c60174515
Meettya <meettya@gmail.com>
2011-11-06 14:43:55 +0300

    * some small fix

--- update_doc.sh
+++ update_doc.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-doc_dir='doc'
-tmp_message='tmp_mess'
-parent_sha=$(git show-ref -s refs/heads/gh-pages)
+
+doc_dir='doc' # document directory
+tmp_message='tmp_mess' # temporary files for changelog message
+gh_pages='refs/heads/gh-pages' # refs to pages
+
+parent_sha=$(git show-ref -s $gh_pages)
 doc_sha=$(git ls-tree -d HEAD $doc_dir | awk '{print $3}')
 git log --pretty=format:'%s' -n 1 $doc_dir > $tmp_message
-new_commit=$(echo "Auto-update docs." | git commit-tree $doc_sha -p $parent_sha < $tmp_message )
+new_commit=$(git commit-tree $doc_sha -p $parent_sha < $tmp_message )
 rm $tmp_message
-git update-ref refs/heads/gh-pages $new_commit
+git update-ref $gh_pages $new_commit
+
+echo "Docs update done"
